10334a62732f302c2a96e791413c6ab53dac9246
Breno Dias Santos <160603341+brenodiassto@users.noreply.github.com>
2024-03-14 14:18:53 +0000

    Criando o arquivo de doperadores aritimetica

---
+++ operadores-aritimetica.js
@@ -0,0 +1,24 @@
+const numero1 = 40;
+const numero2 = 20;
+
+const soma = numero1 + numero2;
+
+const subtracao = numero1 - numero2;
+
+const multiplicacao = numero1 * numero2;
+
+const divisao = numero1 / numero2;
+
+const resto = numero1 % numero2;
+
+
+
+console.log(`Os números das operações são ${numero1} e ${numero2}`);
+console.log(`A soma dos números é ${numero1 + numero2}`);
+console.log(`A subtracao dos números é ${numero1 - numero2}`);
+console.log(`A divisao dos números é ${numero1 / numero2}`);
+console.log(`A multiplicacao dos números é ${numero1 * numero2}`);
+console.log(`O resto ds divisao dos números é ${numero1 % numero2}`);
+
+
+
