b4151a85577bd7b4bc860fe8e54824ba8ae0162a
Breno Dias Santos <160603341+brenodiassto@users.noreply.github.com>
2024-03-17 16:42:01 +0000

    Atualizando a calculadora IMC

--- desafio2.js
+++ desafio2.js
@@ -1,24 +1,27 @@
 //Crie um programa que calcule o Índice de Massa Corporal (IMC) de uma pessoa.
 //Orientações:
 
-//Utilize o método prompt() do objeto window para realizar a leitura dos dados de entrada.
-//Utilize a fórmula IMC = peso / altura² para calcular o IMC.
-//Utilize a estrutura if-else para classificar o IMC de acordo com a tabela da OMS:
-//IMC	Indicação
-//Abaixo de 18,5	Abaixo do peso
-//18,5 - 24,9	Peso normal
-25,0 - 29,9	Sobrepeso
-30,0 - 34,9	Obesidade grau I
-35,0 - 39,9	Obesidade grau II
-Acima de 40,0	Obesidade grau III
+//Utilize o método prompt() do objeto window para realizar 
+//a leitura dos dados de entrada//.
+//Utilize a fórmula IMC = peso / altura² para calcular o IMC.//
+//Utilize a estrutura if-else para classificar o IMC de 
+//acordo com a tabela da OMS:
 
+////IMC	Indicação
+////Abaixo de 18,5	Abaixo do peso
+//18,5 - 24,9	Peso normal
+//25,0 - 29,9	Sobrepeso
+//30,0 - 34,9	Obesidade grau I
+//35,0 - 39,9	Obesidade grau II
+//Acima de 40,0	Obesidade grau III
 
 
 const 
 
-window.prompt('');
+window.prompt('Tabela IMC');
 
 if(<= 18.5) {
     console.log('Abaixo do Peso')
 };
+
 
